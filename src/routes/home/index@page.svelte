4589018b0cceee7a55c601253e0e2374544f255e
adamaho <adam.aho@hey.com>
2022-07-06 08:04:39 -0400

    removing networks from arctic wolf

--- src/routes/home/index@page.svelte
+++ src/routes/home/index@page.svelte
@@ -4,9 +4,8 @@
 <hr class="separator" />
 
 <p class="shpeel">
-	Currently ending cyber risk at <a href="https://arcticwolf.com" target="_blank"
-		>Arctic Wolf Networks</a
-	>. Previously at <a href="https://ottomotors.com" target="_blank">OTTO Motors</a>.
+	Currently ending cyber risk at <a href="https://arcticwolf.com" target="_blank">Arctic Wolf</a>.
+	Previously at <a href="https://ottomotors.com" target="_blank">OTTO Motors</a>.
 </p>
 
 <style>
