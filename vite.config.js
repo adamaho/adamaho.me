eb678e1ec38b21bc0d538c38b707df8bf4295a5c
adamaho <adam.aho@hey.com>
2022-07-24 15:59:59 -0400

    adding some more stuff

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,16 @@ import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-    plugins: [sveltekit(), vanillaExtractPlugin()],
-    resolve: {
-        alias: {
-            '~/styles': path.resolve('./src/styles'),
-            '~/components': path.resolve('./src/components'),
-            '~/stores': path.resolve('./src/stores'),
-            '~/lib': path.resolve('./src/lib'),
-            '~/constants': path.resolve('./src/constants')
-        }
-    }
+	plugins: [sveltekit(), vanillaExtractPlugin()],
+	resolve: {
+		alias: {
+			'~/styles': path.resolve('./src/styles'),
+			'~/components': path.resolve('./src/components'),
+			'~/stores': path.resolve('./src/stores'),
+			'~/lib': path.resolve('./src/lib'),
+			'~/constants': path.resolve('./src/constants')
+		}
+	}
 };
 
 export default config;
