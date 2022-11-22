2ce1de420c02a73cf45ac6e89ef5ee498f957fd4
adamaho <adam.aho@hey.com>
2022-11-22 17:51:07 -0500

    adding some more stuff

--- src/routes/bullet/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import PageHeading from '~/components/PageHeading.svelte';
-</script>
-
-<PageHeading title="Bullet">
-	<svelte:fragment slot="description">A place to organize your life.</svelte:fragment>
-</PageHeading>
-
-<p>⚠️ A work in progress.</p>
